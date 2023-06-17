bf5ebb39101c9b4b94195c6127ec2b37460700a8
Kiyzan <mzpnzakky@gmail.com>
2023-06-17 11:04:37 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
 
 const initialValue = document.getElementById("input")
 const finalValue = document.getElementById("result")
-const button = document.getElementById("btn-result")
+const calcBtn = document.getElementById("btn-result")
+const resetBtn = document.getElementById("reset")
+const num = document.getElementById("num")
 
 const celsiusToFahrenheit = (cel => {
-    let fahrenheit = (cel * 9/5) + 32
-    return fahrenheit
-})
+    let fahrenheit = (cel * 9/5) + 32 // cari nilai cel (celsius) lalu lakukan aritmatika
+    return fahrenheit // kembalikan nilai fahrenheit (nilai dari aritmatika)
+}) // melakukan kalkulasi
     
 const fahrenheitToCelsius = (fah => {
-    let celsius = (fah - 32) * 5/9
-    return celsius
+    let celsius = (fah - 32) * 5/9 // cari nilai fah (fahrenheit) lalu lakukan aritmatika
+    return celsius // kembalikan nilai celsius (nilai dari aritmatika)
+}) // melakukan kalkulasi
+
+calcBtn.addEventListener("click", () => {
+    finalValue.value = `${celsiusToFahrenheit(initialValue.value)}` // lakukan kalkulasi dari nilai Initial.value melalui function celsiusToFahrenheit
+    num.innerHTML = `${initialValue.value}` // dapatkan dan isi variabel num dengan initialValue.value untuk mengisi formula
 })
 
-button.addEventListener("click", () => {
-    finalValue.value = `${celsiusToFahrenheit(initialValue.value)}`
+resetBtn.addEventListener("click", () => {
+    initialValue.value = `0` // reset value atau nilai dari kolom
+    finalValue.value = `` // hapus kesuluruhan value atau nilai dari kolom
+    num.innerHTML = `0` // reset value atau nilai dari paragraph
 })
